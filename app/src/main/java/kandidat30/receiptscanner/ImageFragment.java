bbfb36dd99166f1e77400dda2a3adf379ab04cb6
Jacobth <jacob.thureson@gmail.com>
2017-04-01 12:09:43 +0200

    chaned theme

--- app/src/main/java/kandidat30/receiptscanner/ImageFragment.java
+++ app/src/main/java/kandidat30/receiptscanner/ImageFragment.java
@@ -20,8 +20,7 @@ import android.widget.ImageView;
  * create an instance of this fragment.
  */
 public class ImageFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "pos";
 
     private int pos;
